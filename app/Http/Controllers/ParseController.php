f0104d55aef6d0640e8aac661d0cfe78a56e34a2
apmarev <57302841+apmarev@users.noreply.github.com>
2023-05-19 16:41:48 +0200

    init

--- app/Http/Controllers/ParseController.php
+++ app/Http/Controllers/ParseController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Revolution\Google\Sheets\Facades\Sheets;
 
 class ParseController extends Controller {
 
-    public function getMessage(Request $request) {
+    public function getMessage(Request $request): string {
 
         if($request->has('message') && isset($request->input('message')['text'])) {
             $message = [];
@@ -17,9 +16,7 @@ class ParseController extends Controller {
 
             foreach($lines as $line) {
                 if($line == '' || $line == ' ') continue;
-                if(strripos($line, '[') === false) {
-
-                } else continue;
+                if(strripos($line, '[') === false) {} else continue;
 
                 $string = trim(preg_replace("/[^a-zA-ZА-Яа-яЁё0-9\s]/u","", $line));
 
@@ -38,10 +35,10 @@ class ParseController extends Controller {
                     ];
             }
 
-
-
-
-            $values = Sheets::spreadsheet('160Fn0hUuonM4AvH1sdc92o1qOCv5NK2KNvhDAGUFR8I')->sheet('Лист1')->range('A1:B1000')->all();
+            $values = Sheets::spreadsheet(config('app.google_table_id'))
+                ->sheet(config('app.google_table_list_name'))
+                ->range('A1:B1000')
+                ->all();
 
             $insert = [];
 
@@ -60,18 +57,14 @@ class ParseController extends Controller {
                 else $insert[] = [$device['name'], $device['price']];
             }
 
-            if(sizeof($insert) > 0) {
-                foreach($insert as $i) {
-                    $values[] = $i;
-                }
-            }
+            if(sizeof($insert) > 0) foreach($insert as $i) $values[] = $i;
 
-            Sheets::spreadsheet('160Fn0hUuonM4AvH1sdc92o1qOCv5NK2KNvhDAGUFR8I')->sheet('Лист1')->range("A1")->update($values);
+            Sheets::spreadsheet(config('app.google_table_id'))
+                ->sheet(config('app.google_table_list_name'))
+                ->range("A1")
+                ->update($values);
 
         }
-
-
-
 
         return "Ok";
 
--- config/app.php
+++ config/app.php
@@ -5,18 +5,10 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'google_table_id' => env('GOOGLE_TABLE_ID', ''),
+    'google_table_list_name' => env('GOOGLE_TABLE_LIST_NAME', 'Лист1'),
 
     /*
     |--------------------------------------------------------------------------
